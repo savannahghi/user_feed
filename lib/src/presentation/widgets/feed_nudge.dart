18c4ed072ca93ae748dc949c42978123e1099924
maiyo <maiyo.kevin@healthcloud.co.ke>
2021-06-21 15:37:46 +0300

    chore: ui polish

--- lib/src/presentation/widgets/feed_nudge.dart
+++ lib/src/presentation/widgets/feed_nudge.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:user_feed/src/domain/entities/action.dart' as feed_action;
 import 'package:user_feed/src/domain/entities/nudge.dart';
-import 'package:user_feed/src/domain/value_objects/colors.dart';
 import 'package:user_feed/src/presentation/widgets/feed_action_buttons.dart';
 
 import 'package:shared_themes/spaces.dart';
@@ -22,51 +21,58 @@ class FeedNudge extends StatelessWidget {
 
     final List<feed_action.Action> nudgeActions = nudge.actions!;
 
+    const BorderRadius radius = BorderRadius.all(Radius.circular(8));
+
     return Stack(
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(15),
+        Positioned.fill(
           child: Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: nudgeBackgroundColor,
-              borderRadius: BorderRadius.all(Radius.circular(8)),
+            decoration: BoxDecoration(
+              borderRadius: radius,
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Theme.of(context).accentColor.withRed(130).withOpacity(0.8),
+                  Theme.of(context).accentColor.withBlue(100),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            padding: const EdgeInsets.only(left: 15, bottom: 15, right: 15),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Text(title, style: TextThemes.boldSize18Text()),
-                smallVerticalSizedBox,
-                Text(text, style: TextThemes.normalSize14Text()),
-                smallVerticalSizedBox,
+        Container(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+          decoration: const BoxDecoration(
+            borderRadius: radius,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title,
+                style: TextThemes.boldSize18Text(Colors.white),
+              ),
+              smallVerticalSizedBox,
+              Text(
+                text,
+                style: TextThemes.normalSize15Text(Colors.white),
+              ),
+              smallVerticalSizedBox,
 
-                // todo(future) - check if nudge actions are null
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    ...nudgeActions
-                        .map(
-                          (feed_action.Action nudgeAction) => FeedActionButton(
-                            key: Key(nudgeAction.id!),
-                            action: nudgeAction,
-                          ),
-                        )
-                        .toList()
-                  ],
-                )
-              ],
-            ),
+              // todo(future) - check if nudge actions are null
+              Row(
+                children: <Widget>[
+                  ...nudgeActions
+                      .map(
+                        (feed_action.Action nudgeAction) => FeedActionButton(
+                          key: Key(nudgeAction.id!),
+                          action: nudgeAction,
+                        ),
+                      )
+                      .toList()
+                ],
+              ),
+            ],
           ),
         ),
       ],
