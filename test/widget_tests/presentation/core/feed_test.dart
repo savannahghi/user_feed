2821bb70d9d04d9352761bf4ff941f78463d2370
Steven Maina <maichstivo254@gmail.com>
2021-07-13 17:34:23 +0300

    chore: extract buy cover nudge to consumer (#15)

--- test/widget_tests/presentation/core/feed_test.dart
+++ test/widget_tests/presentation/core/feed_test.dart
@@ -1,14 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:user_feed/src/domain/resources/inputs.dart';
-import 'package:user_feed/src/domain/value_objects/constants.dart';
 import 'package:user_feed/src/domain/value_objects/enums.dart';
 import 'package:user_feed/src/domain/value_objects/strings.dart';
 import 'package:user_feed/src/domain/value_objects/widget_keys.dart';
 import 'package:user_feed/src/presentation/core/feed.dart';
 import 'package:user_feed/src/presentation/widgets/feed_cover_mini_card.dart';
-import 'package:user_feed/src/presentation/widgets/feed_item_cover_action.dart';
-import 'package:shared_ui_components/buttons.dart';
 
 import '../../../mock_data.dart';
 import '../../../mock_utils.dart';
@@ -120,34 +117,6 @@ void main() {
             });
 
             expect(find.text(completeProfileTitle), findsOneWidget);
-          },
-        );
-      },
-    );
-
-    testWidgets(
-      'should render correctly cover call to action',
-      (WidgetTester tester) async {
-        await tester.runAsync(
-          () async {
-            await mockNetworkImages(() async {
-              await buildTestWidget(
-                tester: tester,
-                child: FeedComponent(
-                  userFeed: FeedResponsePayload.fromJson(
-                      mockFeedResponsePayload(<Map<String, dynamic>>[])),
-                  flavour: Flavour.CONSUMER,
-                  feedContentCallbacks: mockGetFeedActionCallbacks(),
-                  isSmallScreen: true,
-                  covers: const <dynamic>[],
-                ),
-              );
-            });
-
-            expect(find.byType(FeedItemCoverCallToAction), findsOneWidget);
-            expect(find.text(coverCallToActionTitle), findsOneWidget);
-
-            await tester.tap(find.byType(SILPrimaryButton));
           },
         );
       },
